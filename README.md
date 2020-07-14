0324c57d195de7cb2fdcd9bf8156ff0b63e6e5fb
chenweiyan <chanweiyan007@gmail.com>
2020-07-14 17:07:14 +0800

    config loader

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 ```bash
 npm init -y
+-y 创建package.json时使用默认值
+
 npm i webpack webpack-cli -D
 -D, --save-dev
 -S, --save
+
 ./node_modules/.bin/webpack -d
 -d           shortcut for --debug --devtool eval-cheap-module-source-map
+
+browser-sync start --server --browser "Google Chrome" --files "stylesheets/*.css, *.html, *.js"
+
+npm install -D babel-core babel-loader@7 babel-preset-env babel-preset-react
 
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 var webpack = require('webpack');
 var path = require('path');
-var App_DIR = path.resolve(__dirname, 'src');
-var BUILD_DIR = path.resolve(__dirname, 'build');
+var App_DIR = path.resolve(__dirname, 'src');     // 项目路径
+var BUILD_DIR = path.resolve(__dirname, 'build'); // 构建路径
 var config = {
-  entry: App_DIR + '/index.jsx', // 入口
+  entry: App_DIR + '/index.jsx', // 项目入口
   output: {
-    path: BUILD_DIR,       // 出口路径
-    filename: 'bundle.js'  // 出口文件名
+    path: BUILD_DIR,             // 输出路径
+    filename: 'bundle.js'        // 输出文件命名
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,      // 编译后缀为 js 和 jsx 格式文件
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"  // 使用 babel-loader 这个 loader 库
+        }
+      }
+    ]
   }
 };
 
